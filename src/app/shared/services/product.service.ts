20700f0d975ebc54d6465d807d32340e407dbe0f
barbaracalderon <bcalderoni.ti@gmail.com>
2024-07-20 16:36:17 -0300

    refactor(search): display products with image and from home component

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, of } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   private products = [
-    { id: 1, name: 'Café', description: 'Café forte', price: 10 },
-    { id: 2, name: 'Açúcar', description: 'Açúcar refinado', price: 5 },
+    { id: 1, name: 'Produto 1', description: 'O primeiro produto da lojinha', price: 100, imageUrl: 'assets/product1.jpg' },
+    { id: 2, name: 'Produto 2', description: 'O segundo produto da lojinha', price: 70, imageUrl: 'assets/product2.jpg' },
   ];
   
   private searchResults = new BehaviorSubject<any[]>([]);
