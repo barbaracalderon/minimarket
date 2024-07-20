8a539b384ef4342b9ad8af1fce4f391d8f8b2a61
Eder Sparenberger <edsparen@gmail.com>
2024-07-20 15:22:18 -0300

    chore: organizes the product components to a feature folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductViewComponent } from './features/products/components/product-view/product-view.component';
 import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
