cd32aa805e045706b8ae8b69bd83bc472bbfcbb0
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-07-21 18:17:57 -0300

    Ex. 05 - Listagem de produtos

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductService } from '../shared/services/product.service';
 import { CommonModule } from '@angular/common';
 
@@ -6,15 +7,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.scss'],
-  imports: [CommonModule],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
 export class SearchResultComponent {
   searchResults: any[] = [];
+  noResults: boolean = false;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private router: Router) {
     this.productService.searchResults$.subscribe(results => {
       this.searchResults = results;
+      this.noResults = results.length === 0;
     });
+  }
+
+  viewProduct(productId: number) {
+    this.router.navigate(['/product', productId]);
   }
 }
